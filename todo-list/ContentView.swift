027a4dccc509af86f52ec628bc08966b14bf5c27
Inoue-KK <kyo.yuzukko.99@gmail.com>
2026-04-07 20:56:36 +0900

    refactor(haptics): merge duplicate playLightHaptic into playHaptic

--- todo-list/ContentView.swift
+++ todo-list/ContentView.swift
@@ -109,7 +109,7 @@ struct ContentView: View {
     private func todoRow(_ todo: Todo) -> some View {
         HStack(spacing: 14) {
             TodoCheckboxButton(todo: todo) {
-                if hapticEnabled { playLightHaptic() }
+                if hapticEnabled { playHaptic() }
             } onRelease: {
                 let completing = !todo.isCompleted
                 withAnimation(.spring(duration: 0.3)) {
@@ -184,17 +184,6 @@ struct ContentView: View {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         hapticEngine = try? CHHapticEngine()
         try? hapticEngine?.start()
-    }
-
-    private func playLightHaptic() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1.0)
-        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-        guard let pattern = try? CHHapticPattern(events: [event], parameters: []),
-              let player = try? hapticEngine?.makePlayer(with: pattern) else { return }
-        try? hapticEngine?.start()
-        try? player.start(atTime: CHHapticTimeImmediate)
     }
 
     private func playHaptic() {
